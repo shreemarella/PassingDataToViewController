cb4559f32ecae7d2ec4ef01918d27da992ec85dc
shreemarella <mvsrikanth96@gmail.com>
2020-03-03 18:23:13 -0500

    Initial Commit

---
+++ PassingDataToViewController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PassingDataToViewController
+//
+//  Created by Shree Marella on 2020-03-03.
+//  Copyright © 2020 Shree Marella. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
